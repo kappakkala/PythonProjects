3ea510d30437ecd9677961234e4ec5452de1e20a
kappakkaala <69795698+kappakkaala@users.noreply.github.com>
2020-08-17 13:03:43 +0200

    basic blink using built-in leds

---
+++ micropython/blink_8266.py
@@ -0,0 +1,16 @@
+from machine import Pin
+from time import sleep
+
+LED_BUILTIN1 = 2
+LED_BUILTIN2 = 16
+
+led1 = Pin(LED_BUILTIN1, Pin.OUT)
+led2 = Pin(LED_BUILTIN2, Pin.OUT)
+
+while True:
+    led1.on()
+    led2.off()
+    sleep(1)
+    led1.off()
+    led2.on()
+    sleep(1)
