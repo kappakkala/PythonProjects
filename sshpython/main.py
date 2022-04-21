4a2bac917fa9d82fcda2fd251499e5398ae3503d
pakkaran <thto1013@gmail.com>
2022-04-21 14:29:11 +0200

    ssh copy directory and files
    create subdirectory if not exists

--- sshpython/main.py
+++ sshpython/main.py
@@ -1,34 +1,38 @@
 import paramiko
 import yaml
 import os
+import glob
 
 # gets the current active location of the file
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
+with open(__location__+'\settings.yaml') as f:
+    settings = yaml.safe_load(f)
 
-with open(__location__+'\credentials.yaml') as f:
-    credentials = yaml.safe_load(f)
+host1, user1, password1, remote_dir, local_dir = settings['host1'], settings['username1'], settings['password1'], settings['remote_dir2'], settings['local_dir']
+# gets all files inside local_dir and subfolders inside local_dir
+local_files = glob.glob(local_dir+'/*/*.*') + glob.glob(local_dir+'/*.*')
 
-
-host1, user1, password1, source_directory = credentials['host1'], credentials['username1'], credentials['password1'], credentials['source_directory2']
-
-transport = paramiko.Transport((host1, 22))
-transport.connect(username =user1, password = password1)
-sftp = paramiko.SFTPClient.from_transport(transport)
-try:
-    # creates a directory if it doesn't exists
-    sftp.mkdir(source_directory)
-except OSError:
-    pass
-inbound_files=sftp.listdir(source_directory)
-sftp.close()
-transport.close()
-
+# ssh client
 ssh = paramiko.SSHClient() 
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(host1, username=user1, password=password1)
 sftp = ssh.open_sftp()
-# copying a single file to the destination
-sftp.put(__location__+'\credentials.yaml', source_directory+'credentials.yaml', callback=None)
+
+# copying files from local dir to destination dir
+for local_file in local_files:
+    # filename with folders
+    file = local_file.split(local_dir)[-1]
+    # changing the path format : windows format to linux
+    file = file.replace('\\','/')
+    remote_file = remote_dir + file
+    # create remote subdir if doesn't exist
+    try:
+        sftp.mkdir(('/').join(remote_file.split('/')[:-1]))
+    except OSError:
+        pass
+    sftp.put(local_file, remote_file, callback=None)
+    print(remote_file+ ' copied succesfully')
+# close existing connections
 sftp.close()
 ssh.close()
