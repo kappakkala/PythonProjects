74bba8c048d38cb337b4533ef23a115fb59af2fc
pakkaran <thto1013@gmail.com>
2022-04-27 12:21:01 +0200

    writing test data

--- pyinflux/main.py
+++ pyinflux/main.py
@@ -1,13 +1,21 @@
 import pyinfluxops
 
-
 def main():
     # create a class object
     pyflux = pyinfluxops.Influxops()
+    db = 'test'
+    # lists exisiting databases
+    pyflux.list_databases()
     # create a new database
-    pyflux.create_database("test")
+    pyflux.create_database(db)
+    # lists exisiting measurements from database
+    pyflux.list_measurements(db)
+    # generates a test dataframe object
+    pyflux.generate_test_dataframe()
+    # write df to influx
+    pyflux.write_data(db=db, df=pyflux.df, measurement_write='test_measurement', influx_tags={'testfield':'type1'}, influx_keys = ['hobby'])
     # delete a database
-    pyflux.drop_database("test")
+    pyflux.drop_database(db)
     # close connection
     pyflux.close_connection()
 
--- pyinflux/pyinfluxops.py
+++ pyinflux/pyinfluxops.py
@@ -1,7 +1,9 @@
 import os
 from influxdb import DataFrameClient
 import yaml
-import pandas
+from datetime import datetime, timedelta
+from dateutil.parser import parse
+import pandas as pd
 
 
 class Influxops:
@@ -50,6 +52,8 @@ class Influxops:
             print(f"Database {database} is created.")
 
     def drop_database(self, database):
+        # get list of existing databases
+        self.list_databases(display=False)
         # check if database already exists
         if database not in self.list_db:
             print(f"Database {database} does not exist. No database is deleted.")
@@ -58,7 +62,41 @@ class Influxops:
             self.client.drop_database(database)
             print(f"Database {database} is deleted.")
 
+    def list_measurements(self, db, display=True):
+        # select the database
+        self.client.switch_database(db)
+        # list measurements
+        list_measurements = self.client.get_list_measurements()
+        if len(list_measurements)!=0:
+            # list_measurements is a list of dicts with key 'name'
+            self.list_measurements = [i["name"] for i in list_measurements]
+        else:
+            self.list_measurements = list_measurements
+        if display:
+            print(self.list_measurements)
+    
+    def write_data(self, db, df, measurement_write, influx_tags, influx_keys=None):
+        # writes a pandas dataframe df into an influx measurement measurement_write using tags from dict influx_tags and list influx_keys
+        # influx_tags is an identifier that is additional to df.columns 
+        # influx_keys contains a list which is a subset of df.columns
+        # select the database
+        self.client.switch_database(db)
+        # write data from df
+        if influx_keys == None:
+            status = self.client.write_points(df, measurement_write, tags=influx_tags, protocol='line', batch_size=1000)
+        else:
+            status = self.client.write_points(df, measurement_write, tags=influx_tags, tag_columns=influx_keys, protocol='line', batch_size=1000)
+        print(status)
+
     def close_connection(self):
         # closes the client connection
         self.client.close()
         print("Closing connection")
+
+    def generate_test_dataframe(self):
+        # initialize data of lists.
+        data = {'name':['Tom', 'nick', 'krish', 'jack'],
+                'age':[20, 21, 19, 18],
+                'hobby': ['swimming', 'football', 'fencing', 'swimming']}
+        self.df = pd.DataFrame(data=data)
+        self.df.index = [parse('2022-01-13 17:00:00')+timedelta(minutes=i) for i in range(len(self.df))]
